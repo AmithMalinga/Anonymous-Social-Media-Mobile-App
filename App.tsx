c974b0b412e44434436224d6bf3d861ed444680c
K Amith Malinga <malingaamith1@gmail.com>
2024-03-12 00:43:25 +0530

    initial commit replaced

--- App.tsx
+++ App.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
-import FeelWings from './src/components/FeelWingsLogo';
+import { TextInput, Text, View } from 'react-native';
+import MainFont from './src/components/MainFont';
 
 function App(): React.JSX.Element {
-  
   return (
-    <View style={{alignItems:'center'}}>
-      <Text>Welcome</Text>
-      <FeelWings/>
+    <View style={{ alignItems: 'center' }}>
+      <MainFont>Hello Anonymous!</MainFont>
+      
+
+      
+      <TextInput
+        style={{
+          fontFamily: 'Cagliostro',
+          borderWidth: 1,
+          borderColor: 'black',
+          borderRadius: 5,
+          width: 250,
+          marginTop: 4,
+          height: 40,
+          paddingLeft: 20,
+          fontSize: 12,
+        }}
+        placeholder="Email"
+      />
+      
+      
+      <TextInput
+        style={{
+          borderWidth: 1,
+          borderColor: 'blue',
+          borderRadius: 5,
+          width: 250,
+          marginTop: 4,
+          height: 40,
+          paddingLeft: 20,
+          fontSize: 12,
+        }}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
     </View>
   );
 }
-
-
 
 export default App;
