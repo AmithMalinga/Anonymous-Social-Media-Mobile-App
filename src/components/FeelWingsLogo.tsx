889570bbc6869dc09b913f17ce50bb9f2eb38b2d
K Amith Malinga <malingaamith1@gmail.com>
2024-03-11 21:59:49 +0530

    first commit

---
+++ src/components/FeelWingsLogo.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const FeelWings = () => {
+  return (
+    <View>
+      <Text style={{ color: '#666666', fontFamily: 'Cagliostro', fontSize: 21 }}>
+        FeelWings
+      </Text>
+    </View>
+  );
+};
+
+export default FeelWings;
