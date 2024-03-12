30ffaa9897646009a2063cb43dd76c3b1af4cb9f
K Amith Malinga <malingaamith1@gmail.com>
2024-03-12 16:36:04 +0530

    login page styled and login button added

--- App.tsx
+++ App.tsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import { TextInput, Text, View } from 'react-native';
+import { TextInput, Text, View, TouchableOpacity, StyleSheet } from 'react-native'; // Added imports
 import MainFont from './src/components/MainFont';
 
 function App(): React.JSX.Element {
   return (
-    <View style={{ alignItems: 'center' }}>
-      <MainFont>Hello Anonymous!</MainFont>
+    <View style={styles.container}>
       
+      {/* <MainFont>Hello Anonymous!</MainFont> */}
 
-      
       <TextInput
         style={{
           fontFamily: 'Cagliostro',
           borderWidth: 1,
-          borderColor: 'black',
+          borderColor: '#4DA5B9',
           borderRadius: 5,
           width: 250,
-          marginTop: 4,
+          marginTop: 250,
           height: 40,
           paddingLeft: 20,
           fontSize: 12,
         }}
         placeholder="Email"
       />
-      
-      
+
       <TextInput
         style={{
           borderWidth: 1,
-          borderColor: 'blue',
+          borderColor: '#4DA5B9',
           borderRadius: 5,
           width: 250,
-          marginTop: 4,
+          marginTop: 10,
           height: 40,
           paddingLeft: 20,
           fontSize: 12,
@@ -39,8 +37,36 @@ function App(): React.JSX.Element {
         placeholder="Password"
         secureTextEntry={true}
       />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          // Handle login logic here
+        }}
+      >
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#4DA5B9',
+    borderRadius: 5,
+    marginTop: 10,
+    padding: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    width:232,
+  },
+});
 
 export default App;
